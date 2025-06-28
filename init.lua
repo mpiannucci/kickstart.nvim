7299eee60fb147d044e9b9e7861f604b90791d40
Matthew Iannucci <mpiannucci@gmail.com>
2025-06-28 12:30:26 -0400

    Fix formatting

--- init.lua
+++ init.lua
@@ -749,7 +749,7 @@ require('lazy').setup({
 
   { -- Autoformat
     'stevearc/conform.nvim',
-    enabled = false,
+    enabled = true,
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
     keys = {
@@ -763,28 +763,37 @@ require('lazy').setup({
       },
     },
     opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
+      notify_on_error = true,
+      -- Disable "Format on save"
+      format_on_save = false,
+      --format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
+      --  local disable_filetypes = { c = true, cpp = true }
+      --  if disable_filetypes[vim.bo[bufnr].filetype] then
+      --    return nil
+      --  else
+      --    return {
+      --      timeout_ms = 500,
+      --      lsp_format = 'fallback',
+      --    }
+      --  end
+      -- end,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { "ruff", "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        css = { "prettierd", "prettier", stop_after_first = true },
+        html = { "prettierd", "prettier", stop_after_first = true },
+        json = { "prettierd", "prettier", stop_after_first = true },
+        yaml = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
