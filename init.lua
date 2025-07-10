09d39403d05a7e1bec2bd7ecdc56c15485af186a
Matthew Iannucci <mpiannucci@gmail.com>
2025-07-10 13:05:39 -0400

    Add toggleterm

--- init.lua
+++ init.lua
@@ -994,6 +994,16 @@ require('lazy').setup({
     },
   },
 
+
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {
+      open_mapping = "<leader>tt",
+      direction = 'horizontal',
+    },
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
