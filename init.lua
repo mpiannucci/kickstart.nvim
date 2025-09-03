3b12a894bd7e311235c81926381293b1ba3c3470
Matthew Iannucci <mpiannucci@gmail.com>
2025-09-03 09:53:13 -0400

    Add go to diagnostic keybind

--- init.lua
+++ init.lua
@@ -601,6 +601,9 @@ require('lazy').setup({
           --  the definition of its *type*, not where it was *defined*.
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
+          --- Float the dianostic of the current line
+          map('gd', vim.diagnostic.open_float, '[G]oto [D]iagnostic')
+
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
           ---@param client vim.lsp.Client
           ---@param method vim.lsp.protocol.Method
